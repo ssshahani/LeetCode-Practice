d9c43acb11e21844c62734afa715cff37838fa23
ssshahani <sahilsam1989@hotmail.com>
2018-08-30 00:52:16 -0400

    Handled condition

--- IntegerToWords.py
+++ IntegerToWords.py
@@ -46,6 +46,7 @@ class IntegerToWords:
         keys = [1000000000,1000000,1000,100,90,80,70,60,50,40,30,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]
         
         def internal_func(n):
+            if n <= 20: return word_dict[n]        
             for div in keys:
                 q,r = divmod(n,div)
                 if not q: continue
